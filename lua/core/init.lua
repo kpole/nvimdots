db4e394c1f7cdcc399702ad361bba343314383f3
kpole <751711132@qq.com>
2024-12-03 21:56:26 +0800

    some personal config

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -74,7 +74,7 @@ local disable_distribution_plugins = function()
 end
 
 local leader_map = function()
-	vim.g.mapleader = " "
+	-- vim.g.mapleader = " "
 	-- NOTE:
 	--  > Uncomment the following if you're using a <leader> other than <Space>, and you wish
 	--  > to disable advancing one character by pressing <Space> in normal/visual mode.
